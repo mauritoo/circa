9a1059a9eb28fc6513c0e737086405e4716ef39b
Mauro <mauro@tatatu.com>
2023-08-06 11:04:59 +0200

    Show values as timeline

--- src/components/Today.vue
+++ src/components/Today.vue
@@ -16,56 +16,121 @@
   const store = useSunriseStore();
 
   const propsToShow = [
-    'first_light',
-    'dawn',
-    'sunrise',
-    'solar_noon',
-    'golden_hour',
-    'sunset',
-    'last_light',
-    'day_length',
+    {key: 'first_light', text: 'primera luz', color:"#D35400"},
+    {key: 'dawn', text: 'amanecer', color:'#E67E22'},
+    {key: 'sunrise', text: 'salida del sol', color: '#F39C12'},
+    {key: 'solar_noon', text: 'mediodía solar', color: '#F1C40F'},
+    {key: 'golden_hour', text: 'hora dorada', color: '#F1C40F'},
+    {key: 'sunset', text: 'puesta del sol', color: '#F39C12'},
+    {key: 'last_light', text: 'ultima luz', color:"#D35400"},
+    {key: 'day_length', text: 'duración del día', color:"#c2c2c2"},
   ]
   const formatKey = (key: String) => key.replace('_',' ')
+  const formatTime = (text: String) => {
+    try {
+      const parts = text.split(" ")
+      if (parts.length > 1 && parts[1] === 'PM') {
+        const horaPartida = parts[0].split(':')
+        horaPartida[0] = String(parseInt(horaPartida[0]) + 12)
+        parts[0] = horaPartida.join(':')
+      }
+      return parts[0]
+    } catch (e) {
+      console.log(e)
+      return 'error'
+    }
+  }
+  const getItemClass = (index: number): string =>
+  index === propsToShow.length-1 ? 'last': (index === propsToShow.length-2 ? 'previous' : '')  
 
   const today: ComputedRef<Day> = computed(() => store.today as Day);
-  onMounted(() => {
-    store.fetchTodayInfo();
+  const isLoading: ComputedRef<Boolean> = computed(() => store.isLoading );
+  onMounted(async () => {
+    await store.fetchTodayInfo();
   });
 </script>
 
 <template>
-  <div class="day pa-10">
-    <div class="mb-2 pa-4 title text-center">la Pobla de Vallbona</div>
-    <v-divider class="px-5"></v-divider>
-    <v-list lines="two">
-    <v-list-item
-      v-for="key in propsToShow"
-      :key="key"
-      :title="today[key as keyof Day]"
-      :subtitle="formatKey(key)"
-    ></v-list-item>
-  </v-list>
+
+  <div class="day pa-5">
+    <div class="mb-6 pa-2 title text-center">LA POBLA DE VALLBONA</div>
+    <!-- <v-divider class="px-2"></v-divider> -->
+    <div v-if="isLoading" class="day pa-5 text-center">
+    Cargando...
+  </div>
+    <v-timeline v-else side="end" truncate-line="both">
+      <v-timeline-item
+        v-for="prop, index in propsToShow"
+        :key="prop.key"
+        :dot-color="prop.color"
+        size="15px"
+        :class="getItemClass(index)"
+      >
+      <template v-slot:opposite>
+        <span class="text-overline item-key">{{ formatTime(today[prop.key as keyof Day]) }}</span>
+      </template>
+      <div class="text-body-2 item-text">{{ formatKey(prop.text) }}</div>
+
+
+
+
+        <!-- <div class="d-flex flex-column">
+          <span class="text-overline item-key">{{ formatTime(today[prop.key as keyof Day]) }}</span>
+          <div class="text-body-2 item-text">{{ formatKey(prop.text) }}</div>
+        </div> -->
+    </v-timeline-item>
+    </v-timeline>
+    
+    
+    
+    <!-- SIMPLE LIST -->
+    <!-- <v-divider class="px-2"></v-divider> -->
+    <!-- <v-list lines="two">
+      <v-list-item
+      v-for="prop in propsToShow"
+      :key="prop.key"
+      :title="formatTime(today[prop.key as keyof Day])"
+      :subtitle="formatKey(prop.text)"
+      ></v-list-item>
+    </v-list> -->
   </div>
 </template>
 
 <style scoped>
 .day {
   --tw-bg-opacity: 1;
-  background-color: rgb(175 150 251 / var(--tw-bg-opacity));
+  color: #FFF;
+}
+.item-text {
+  color: rgb(164, 163, 163);
+}
+.item-key {
+  font-size: 14px !important;
+  color: #FFF;
+}
+:deep(.v-timeline-divider__before),:deep(.v-timeline-divider__after) {
+  background: rgb(122, 122, 122);
+}
+:deep(.previous .v-timeline-divider__after),:deep(.last .v-timeline-divider__before) {
+  display: none;
+}
+:deep(.last) {
+  margin-top: 10px;
 }
 .v-list {
-  background-color: transparent;
+  /* background-color: transparent; */
 }
 :deep(.v-list-item-title) {
-  font-weight: bold !important;
+  /* font-weight: bold !important; */
 }
 :deep(.v-list-item-subtitle) {
-  color: white !important;
+  /* font-style: italic; */
+  /* color: white !important; */
 }
 .title {
   --tw-bg-opacity: 1;
-  background-color: rgb(190 255 108 / var(--tw-bg-opacity));
+  /* background-color: rgb(190 255 108 / var(--tw-bg-opacity)); */
   opacity: 1;
-  font-weight: 800;
+  /* font-weight: 800; */
 }
 </style>
--- src/store/sunrise.ts
+++ src/store/sunrise.ts
@@ -6,15 +6,21 @@ export const useSunriseStore = defineStore("sunrise", {
     state: () => ({
         days: [],
         today: {},
+        isLoading: false,
     }),
     getters: {
       getToday(state){
           return state.today
+        },
+      getIsLoading(state){
+          return state.isLoading
         }
     },
     actions: {
       async fetchTodayInfo(lat: number = 39.577185, lng: number = -0.537712) {
         try {
+          this.isLoading = true
+          console.log('fetching......')
           const baseApiUrl = 'https://api.sunrisesunset.io'
           const url = `${baseApiUrl}/json?lat=${lat}&lng=${lng}`
           const response = await axios.get(url)
@@ -22,6 +28,8 @@ export const useSunriseStore = defineStore("sunrise", {
           }
           catch (error) {
             console.log(error)
+        } finally {
+          this.isLoading = false
         }
       }
     },
