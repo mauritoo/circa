99c8edcbdf63ba234318d5e32866e5eaedd4e3df
Mauro <mauro@tatatu.com>
2023-08-07 17:50:30 +0200

    Removed comments

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
-  // import Today from './views/Today.vue'
-  // import SelectPlace from './views/SelectPlace.vue'
 </script>
 
 <template>
-  <!-- <Today/> -->
-  <!-- <SelectPlace/> -->
   <router-view></router-view>
 </template>
 
